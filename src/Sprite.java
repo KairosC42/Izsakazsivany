210a4facb563350900a1f51c1ba585e39d63cc47
devor110 <andras.ballai@gmail.com>
2021-02-26 17:46:15 +0100

    Sprite base class created

---
+++ src/Sprite.java
@@ -0,0 +1,63 @@
+package src;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Rectangle;
+
+public class Sprite
+{
+    protected int x=0;
+    protected int y=0;
+    protected const int width=0;
+    protected const int height=0;
+    protected Image image=null;
+
+    Sprite(int x, int y, int width, int height, Image image)
+    {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.image = image;
+    }
+
+    public void draw(Graphics g)
+    {
+        g.drawImage(image, x, y, width, height, null);
+    }
+
+    public boolean collides(Sprite other)
+    {
+        Rectangle rect = new Rectangle(x, y, width, height);
+        Rectangle otherRect = new Rectangle(other.x, other.y, other.width, other.height);
+        return rect.intersects(otherRect);
+    }
+
+    //getters
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    //setters
+
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
