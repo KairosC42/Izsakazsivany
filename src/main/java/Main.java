e4f1f49a2d86f496719d0aee753ba937242869a7
balintbercy <balint9610@gmail.com>
2021-03-05 18:10:22 +0100

    Generating and creating main JFrame.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,6 @@ public class Main
     {
         System.out.println("Hello World!");
         System.out.println("Bird try hello");
+        MainWindow gui = new MainWindow();
     }
 }
---
+++ src/main/java/MainWindow.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+import java.awt.Dimension;
+
+public class MainWindow
+{
+    private JFrame frame;
+
+
+    public MainWindow() {
+        frame = new JFrame("Labyrinth");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setPreferredSize(new Dimension(800, 850));
+        frame.setResizable(false);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+}
