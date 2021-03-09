f424e13517dac17d57e6bee93ab9238245eab551
thebird97 <balint.mb@gmail.com>
2021-03-10 00:49:42 +0100

    added some enemy variables and methods

--- src/main/java/Enemy.java
+++ src/main/java/Enemy.java
@@ -1,4 +1,36 @@
+import java.awt.*;
+
 public class Enemy extends Sprite
 {
-//todo
+
+
+    public Enemy(int x, int y, int width, int height)
+    {
+        this.x = x;
+        this.y = y;
+        this.width=width;
+        this.height= height;
+      //  this.enemyImages = enemyImages;
+    //    this.image = enemyImages[0];
+    }
+
+
+  private int healthPoints;
+  private float moveSpeed;
+  private int attackRange;
+  private float damage;
+
+    //todo this functions
+  public void behaviour(){
+
+  }
+
+ public void move(){
+
+  }
+  public void attack(){
+
+  }
+
+
 }
