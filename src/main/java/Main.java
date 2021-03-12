96888257b86f19cd93781af53d68c3222bccc835
KairosC42 <kovacs.mate.c42@gmail.com>
2021-03-12 17:38:30 +0100

    Level bővítése visisted adat taggal

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,8 @@ public class Main
         MainWindow gui = new MainWindow();
 
         Level level=new Level(1);
-        level.getRoomMatrix()[level.getStartingRoom().getCoordinate().i][level.getStartingRoom().getCoordinate().i].getRoom();
+        level.printLevel();
+        //level.getRoomMatrix()[level.getStartingRoom().getCoordinate().i][level.getStartingRoom().getCoordinate().i].getRoom();
 
 
     }
