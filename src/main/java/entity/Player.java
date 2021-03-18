0d13c13339b20193372b34b42123d6c9a2244ae7
KairosC42 <kovacs.mate.c42@gmail.com>
2021-03-18 17:49:12 +0100

    equipItem, equipWeapon metódusok kifejtése healthPointMax, attackSpeed, damage adattagok hozzáadása

--- src/main/java/entity/Player.java
+++ src/main/java/entity/Player.java
@@ -3,19 +3,33 @@ package entity;
 import entity.Sprite;
 
 import java.awt.Image;
+import java.util.Vector;
 
 public class Player extends Sprite
 {
+
+
+    /**
+     * Todo:
+     *      -healthPointsMax is a new data,
+     *      -check the usage of healthPoints and replace it with healthPointsMax if necessary
+     *      -revision needed for starting stats
+     *      -potions as a quippedItem? or in a separate Vector?
+     */
+    private int healthPointsMax;
     private int healthPoints;
-    private int experince;
+    private float range = 60;
+    private float attackSpeed = 1;
+    private float damage = 20;
+    private int moveSpeed = 4;
+    private int experince = 0;
     private Directions direction = Directions.Down;
     //private Inventory inventory;
-    private int moveSpeed = 4;
     private int velx;
     private int vely;
     private int money;
-    private int range = 60;
-    //vector<entity.Item> EquippesItems;
+    Vector<StatItem> equippedItems = new Vector<>();
+    Weapon equippedWeapon = new Weapon();
     int walkingTime;
     Image playerImages[];
 
@@ -23,27 +37,27 @@ public class Player extends Sprite
     {
         this.x = x;
         this.y = y;
-        this.width=width;
-        this.height= height;
+        this.width = width;
+        this.height = height;
         this.playerImages = playerImages;
         this.image = playerImages[0];
     }
 
-    public void update(float deltaTime){
-        walkingTime += deltaTime ;
+    public void update(float deltaTime)
+    {
+        walkingTime += deltaTime;
     }
 
     public void moveX()
     {
         if ((velx < 0 && x > 0) || (velx > 0 && x + width <= 900))
         {
-            if(velx>0)
+            if (velx > 0)
             {
                 //jobbra
                 this.image = playerImages[12];
                 direction = Directions.Right;
-            }
-            else if(velx<0)
+            } else if (velx < 0)
             {
                 //balra
                 this.image = playerImages[9];
@@ -57,21 +71,21 @@ public class Player extends Sprite
     {
         if ((vely < 0 && y > 0) || (vely > 0 && y + height <= 600))
         {
-            if(vely>0)
+            if (vely > 0)
             {
                 //előre
-                this.image=this.image = playerImages[0];
+                this.image = this.image = playerImages[0];
                 direction = Directions.Down;
-            }
-            else if(vely<0)
+            } else if (vely < 0)
             {
                 //hátra
-                this.image=this.image = playerImages[4];
+                this.image = this.image = playerImages[4];
                 direction = Directions.Up;
             }
             y += vely;
         }
     }
+
     public int getHealth()
     {
         return this.healthPoints;
@@ -111,11 +125,75 @@ public class Player extends Sprite
     {
         this.moveSpeed = moveSpeed;
     }
-    public Directions getDirection() {
+
+    public Directions getDirection()
+    {
         return direction;
     }
 
-    public int getRange() {
-        return range;
+    public int getHealthPointsMax()
+    {
+        return healthPointsMax;
+    }
+
+    public int getHealthPoints()
+    {
+        return healthPoints;
+    }
+
+    public float getAttackSpeed()
+    {
+        return attackSpeed;
+    }
+
+    public float getDamage()
+    {
+        return damage;
+    }
+
+    public int getExperince()
+    {
+        return experince;
+    }
+
+    public void equipWeapon(Weapon weapon)
+    {
+        range -= equippedWeapon.rangeModifier;
+        attackSpeed -= equippedWeapon.attackSpeedModifier;
+        attackSpeed -= equippedWeapon.damageModifier;
+
+        range += weapon.rangeModifier;
+        attackSpeed += weapon.attackSpeedModifier;
+        attackSpeed += weapon.damageModifier;
+
+        equippedWeapon = weapon;
+
+
+    }
+
+    public void equipItem(StatItem item)
+    {
+        /**
+         * TODO:
+         *      revision: should the health modifier restore health and increase max hp?
+         */
+        healthPointsMax += item.getHealthModifier();
+        healthPoints += item.getHealthModifier();
+
+        range *= item.getRangeModifier();
+        attackSpeed *= item.getAttackSpeedModifier();
+        damage *= item.getDamageModifier();
+        moveSpeed *= item.getSpeedModifier();
+    }
+
+
+
+    public int getRange()
+    {
+        /**
+         * TODO:
+         *      -range int or float?? in Player and every item is a float, in Attack is an int, consensus needed.
+         */
+        return ((int) range);
     }
 }
