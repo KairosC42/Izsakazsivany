1c202683d62d880eba5536df14b573650824caaf
Madár Bálint <mrpblg@inf.elte.hu>
2021-03-23 09:26:00 +0100

    Enemyk mozognak, de nem jól

--- src/main/java/entity/Enemy.java
+++ src/main/java/entity/Enemy.java
@@ -1,15 +1,12 @@
 package entity;
 
-import entity.Sprite;
-
 import java.awt.*;
 import java.util.Random;
 
-public class Enemy extends Sprite
-{
-    private String direction ="left";
+public class Enemy extends Sprite {
+    private String direction = "left";
     private int speed = 2;
-    private String lastMove="left";
+    private String lastMove = "left";
     private int healthPoints;
     private int attackRange;
     private int damage;
@@ -18,46 +15,44 @@ public class Enemy extends Sprite
     private int vely;
 
 
-    public Enemy(int x, int y, int width, int height, Image image)
-    {
+    public Enemy(int x, int y, int width, int height, Image image) {
         super(x, y, width, height, image);
     }
 
 
-
-
     //todo this functions
-    public void behaviour(){
+    public void behaviour() {
 
     }
 
-    public void move(){
+    public void move() {
+
+      //todo move:  if ( x<850 && y<800 ) {   }
+            switch (direction) {
+                case "up":
+                    y -= speed;
+                    break;
+                case "down":
+                    y += speed;
+                    break;
+                case "left":
+                    x -= speed;
+                    break;
+                case "right":
+                    x += speed;
+                    break;
+            }
+            lastMove = direction;
 
 
-        switch (direction) {
-            case "up":
-                y -= speed;
-                break;
-            case "down":
-                y += speed;
-                break;
-            case "left":
-                x -= speed;
-                break;
-            case "right":
-                x += speed;
-                break;
-        }
-        lastMove = direction;
 
     }
 
 
-    public void randDirection()
-    {
+    public void randDirection() {
         Random rand = new Random();
         int randD = rand.nextInt(4);
-        switch(randD){
+        switch (randD) {
             case 0:
                 direction = "up";
                 break;
@@ -75,28 +70,26 @@ public class Enemy extends Sprite
     }
 
 
-    public void moveBack()
-    {
-        switch(lastMove)
-        {
+    public void moveBack() {
+        switch (lastMove) {
             case "up":
-                y+=speed;
+                y += speed;
                 break;
             case "down":
-                y-=speed;
+                y -= speed;
                 break;
             case "left":
-                x+=speed;
+                x += speed;
                 break;
             case "right":
-                x-=speed;
+                x -= speed;
                 break;
         }
     }
-    public void attack(){
+
+    public void attack() {
 
     }
-
 
 
     //Getterek és szetterek az enemyhez
@@ -106,68 +99,68 @@ public class Enemy extends Sprite
         return direction;
     }
 
-    public int getSpeed() {
-        return speed;
-    }
-
-    public String getLastMove() {
-        return lastMove;
-    }
-
-    public int getHealthPoints() {
-        return healthPoints;
-    }
-
-    public int getAttackRange() {
-        return attackRange;
-    }
-
-    public int getDamage() {
-        return damage;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getVelx() {
-        return velx;
-    }
-
-    public int getVely() {
-        return vely;
-    }
-
     public void setDirection(String direction) {
         this.direction = direction;
+    }
+
+    public int getSpeed() {
+        return speed;
     }
 
     public void setSpeed(int speed) {
         this.speed = speed;
     }
 
+    public String getLastMove() {
+        return lastMove;
+    }
+
     public void setLastMove(String lastMove) {
         this.lastMove = lastMove;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
     }
 
     public void setHealthPoints(int healthPoints) {
         this.healthPoints = healthPoints;
     }
 
+    public int getAttackRange() {
+        return attackRange;
+    }
+
     public void setAttackRange(int attackRange) {
         this.attackRange = attackRange;
+    }
+
+    public int getDamage() {
+        return damage;
     }
 
     public void setDamage(int damage) {
         this.damage = damage;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
 
+    public int getVelx() {
+        return velx;
+    }
+
     public void setVelx(int velx) {
         this.velx = velx;
+    }
+
+    public int getVely() {
+        return vely;
     }
 
     public void setVely(int vely) {
