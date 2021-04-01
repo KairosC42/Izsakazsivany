39ac49a2ee4f3eade437057b4ed915cc9bc35812
KairosC42 <kovacs.mate.c42@gmail.com>
2021-04-01 14:51:20 +0200

    Level tesztelés

--- src/main/test/levelLayoutGeneration/LevelTest.java
+++ src/main/test/levelLayoutGeneration/LevelTest.java
@@ -1,6 +1,7 @@
 package levelLayoutGeneration;
 
 import org.junit.jupiter.api.Test;
+import rooms.RoomType;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,16 +12,64 @@ class LevelTest
     void getRoomMatrix()
     {
         Level level = new Level(1);
-        assertNotNull(level.getRoomMatrix());
+        RoomNode[][] roomNodes=level.getRoomMatrix();
+        RoomNode s = level.getStartingRoom();
+        assertNotNull(roomNodes);
+        assertEquals(s,roomNodes[s.getCoordinate().i][s.getCoordinate().j]);
+    }
+    @Test
+    void specialRooms()
+    {
+        int size[]={1,2,3,10,15,30};
+        for (int k = 0; k <size.length ; k++)
+        {
+            Level level=new Level(size[k]);
+            RoomNode[][] roomNodes = level.getRoomMatrix();
+            int numberOfFoundRooms = 0;
+            int numberOfBossRooms = 0;
+            int numberOfItemRooms = 0;
+            int numberOfShopRooms = 0;
+            for (int i = 0; i < roomNodes.length; i++)
+            {
+                for (int j = 0; j < roomNodes[0].length; j++)
+                {
+                    if (roomNodes[i][j] != null)
+                    {
+                        if (roomNodes[i][j].roomType == RoomType.BOSSROOM)
+                        {
+                            numberOfBossRooms++;
+                        }
+                        if (roomNodes[i][j].roomType == RoomType.ITEMROOM)
+                        {
+                            numberOfItemRooms++;
+                        }
+                        if (roomNodes[i][j].roomType == RoomType.SHOP)
+                        {
+                            numberOfShopRooms++;
+                        }
+                        numberOfFoundRooms++;
+                    }
+                }
+
+            }
+            assertEquals(1, numberOfBossRooms);
+            assertEquals(1, numberOfItemRooms);
+            assertEquals(1, numberOfShopRooms);
+            assertEquals(level.getNumberOfRooms(), numberOfFoundRooms);
+        }
     }
 
+
+    
     @Test
-    void getStartingRoom()
+    void startingRoom()
     {
+        Level level = new Level(1);
+        RoomNode s = level.getStartingRoom();
+        assertNotNull(s);
+        assertEquals(RoomType.STARTROOM,s.getRoomType());
+        assertEquals(0,s.getDistanceFromStart());
+
     }
 
-    @Test
-    void getNumberOfRooms()
-    {
-    }
 }
