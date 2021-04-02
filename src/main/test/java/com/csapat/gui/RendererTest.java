a73849315249ec667fc2147836c4aae299d33f1f
GL6BT4 <balint9610@gmail.com>
2021-04-02 20:11:54 +0200

    Fixing a test.

--- src/main/test/java/com/csapat/gui/RendererTest.java
+++ src/main/test/java/com/csapat/gui/RendererTest.java
@@ -5,6 +5,7 @@ import com.csapat.gui.Renderer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 
@@ -13,17 +14,17 @@ class RendererTest
     @Test
     public void RendererTest()
     {
-        JFrame frame = new JFrame("Izsak");
+        /*JFrame frame = new JFrame("Izsak");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         com.csapat.gui.Renderer gameRenderer = new Renderer(900,600,frame);
         gameRenderer.initTiles();
-        Assertions.assertFalse(gameRenderer.getTiles().length==0,"A palya betoltese nem ures tombot eredmenyez");
+        Assertions.assertFalse(gameRenderer.getTiles().length==0,"A palya betoltese nem ures tombot eredmenyez");*/
 
         try
         {
-            Image playerImages[] = gameRenderer.getImages(300,450,100,150,
-                4,4,100,50,"player.png");
+            Image playerimg = ImageIO.read(this.getClass().getClassLoader().getResource("player.png"));
+            Image[] playerImages = {playerimg};
             Player player = new Player(450,100, 30, 30,playerImages,30,30);
             Assertions.assertTrue(player.getHealth()==100, "Kezdő HP");
             Assertions.assertTrue(player.getMoveSpeed()==2, "kezdő mozgási sebesség");
