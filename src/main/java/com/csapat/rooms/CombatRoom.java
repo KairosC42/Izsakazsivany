45e39e2b4991cb3d84632590655f97d67123407d
devor110 <andras.ballai@gmail.com>
2021-04-18 00:55:14 +0200

    actually contains enemies now

--- src/main/java/com/csapat/rooms/CombatRoom.java
+++ src/main/java/com/csapat/rooms/CombatRoom.java
@@ -1,6 +1,12 @@
 package com.csapat.rooms;
 
+import com.csapat.entity.Enemy;
+import com.csapat.entity.Item;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.util.Random;
+import java.util.Vector;
 
 /**
  * <h1>CombatRoom</h1>
@@ -11,35 +17,57 @@ public class CombatRoom extends Room
 
     Random rand= new Random();
     private int enemyCount;
+    private Vector<Item> items;
 
 
     public CombatRoom(int levelDepth)
     {
         super(levelDepth);
-        int enemyCount=rand.nextInt(4)+3;
+        items = new Vector<>();
+        enemyCount=rand.nextInt(4)+3;
+        enemies= new Vector<Enemy>();
         this.roomSpecificGen();
+        this.levelDepth=levelDepth;
     }
 
     @Override
     public void roomSpecificGen()
     {
+        Image enemyTexture=null;
+        try
+        {
+            enemyTexture = ImageIO.read(this.getClass().getClassLoader().getResource("enemy.png"));
+        }
+        catch(Exception e){System.out.println("Enemy texture is missing");}
         for(int i=0;i<enemyCount;++i)
         {
             int healthPoints = rand.nextInt(20)+50 + (levelDepth-1)*15 ;
-            float moveSpeed = 3.2f + (levelDepth-1)*0.4f ;
+            float moveSpeed = 2.5f + (levelDepth-1)*0.4f ;
             int visionRange = rand.nextInt(125)+25 + (levelDepth-1)*50; //enemies have full vision of every room from depth12
             int attackRange = rand.nextInt(20) + 40 + (levelDepth-1)*20 ;
-            float damage = rand.nextInt(10)+20 +(levelDepth-1)*15;
+            int damage = Math.round(rand.nextInt(10)+20 +(levelDepth-1)*15);
 
-            //todo
-            //i have yet to think of how much HP they'll have and how the scaling will look
-            //also the enemy class has yet to be made.
+            enemies.add(new Enemy(/*getN() /2*/ 200 +i*50 ,/*getM()/2*/ 200  ,50,50,enemyTexture,damage,visionRange,attackRange,healthPoints,moveSpeed,levelDepth));
 
 
 
         }
     }
+    public void removeItem(Item item)
+    {
+        for (Item it: items)
+        {
+            if(it.equals(item))
+            {
+                items.remove(item);
+                break;
+            }
+        }
+    }
+
+    public Vector<Item> getItems(){return items;}
     //does a combat room prevent you from leaving while enemies are alive?
     //if so, make that
+
 
 }
